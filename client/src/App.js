46f4143bb694714e878387e64b79dd532344cc42
neerajp99 <neerajp1999@gmail.com>
2019-09-12 04:40:13 +0530

    Initial commit with create react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Ashoka E-Commerce Platform</h1>
+    </div>
+  );
+}
+
+export default App;
