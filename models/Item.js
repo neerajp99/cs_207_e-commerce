8a64fe020286064d9a8546ca755c231ec0f6a20d
neerajp99 <neerajp1999@gmail.com>
2019-10-06 17:00:02 +0530

    Add item model

---
+++ models/Item.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    min: 0,
+    max: 1000000
+  }
+  // details: [
+  //   {
+  //     specialPrice: {
+  //       type: String
+  //     },
+  //     specialDiscount: {
+  //       type: String
+  //     }
+  //   }
+  // ],
+  // extraDetails: [
+  //   {
+  //     highlights: {
+  //       type: String
+  //     },
+  //     services: {
+  //       type: String
+  //     }
+  //   }
+  // ]
+});
+
+module.exports = Item = mongoose.model("items", ItemSchema);
