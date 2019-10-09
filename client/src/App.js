ac3ec11663080605b57260ee714b7eeea263d509
neerajp99 <neerajp1999@gmail.com>
2019-10-09 22:26:54 +0530

    Edit App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
     <div className="app">
-      <h1> Ashoka E-Commerce Platform</h1>
+      <Router>
+        <Route exact path="/" render={() => <Navbar />} />
+        <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/register" render={() => <Register />} />
+      </Router>
+      <h1> </h1>
     </div>
   );
 }
