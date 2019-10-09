3081dfd853ef8a35dd8eb319b9d7da2fb467e174
neerajp99 <neerajp1999@gmail.com>
2019-10-10 03:44:06 +0530

    Add Dashbaor droute

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard"
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route exact path="/" render={() => <Navbar />} />
         <Route exact path="/login" render={() => <Login />} />
         <Route exact path="/register" render={() => <Register />} />
+        <Route exact path="/dashboard" render={() => <Dashboard />} />
+
       </Router>
       <h1> </h1>
     </div>
