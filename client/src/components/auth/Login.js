7217b32bddfb07f0f8b778d530659c14b03fe50e
neerajp99 <neerajp1999@gmail.com>
2019-10-09 22:27:26 +0530

    Add Auth pages

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <h1> LOGIN</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <h1> REGISTR</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
