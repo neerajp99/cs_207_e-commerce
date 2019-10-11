270e1b02d74a5df7791917cd34e1082b7b58789c
neerajp99 <neerajp1999@gmail.com>
2019-10-12 02:14:50 +0530

    Add string check validation

---
+++ validation/checkEmpty.js
@@ -0,0 +1,8 @@
+// function that checks for empty, undefined etc strings
+const checkEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = checkEmpty;
