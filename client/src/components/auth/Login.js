ab99b045768310052c6f94c6d6974eb9fc10e711
neerajp99 <neerajp1999@gmail.com>
2019-10-12 02:13:37 +0530

    Modified Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Navbar from "../Navbar";
 import TextField from "../commons/TextField";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../actions/authActions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class Login extends Component {
   state = {
@@ -11,10 +15,35 @@ class Login extends Component {
     errors: {}
   };
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.loginUser(user, this.props.history);
   };
   render() {
     return (
@@ -25,7 +54,11 @@ class Login extends Component {
             <div class="col-8 center_text m-auto">
               <h1 className="login_heading display-5 text-center"> Login</h1>
               <div className="login_form text-center">
-                <form noValidate className="login_form">
+                <form
+                  noValidate
+                  className="login_form"
+                  onSubmit={this.onSubmit}
+                >
                   <TextField
                     placeholder="ex: tanuj.sood@gmail.com"
                     name="email"
@@ -57,4 +90,18 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(withRouter(Login));
