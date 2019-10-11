ee6f4f595e5f61e623d5e3fd6ee5b68c2f97b6c0
neerajp99 <neerajp1999@gmail.com>
2019-10-12 02:14:11 +0530

    Add function to check of a user is authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import Dashboard from "./components/dashboard/Dashboard"
+import Dashboard from "./components/dashboard/Dashboard";
+import { Provider } from "react-redux";
+import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
-function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Route exact path="/" render={() => <Navbar />} />
-        <Route exact path="/login" render={() => <Login />} />
-        <Route exact path="/register" render={() => <Register />} />
-        <Route exact path="/dashboard" render={() => <Dashboard />} />
+// Check if the user is already authenticated
 
-      </Router>
-      <h1> </h1>
-    </div>
-  );
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode the tokens ang get user infe ormation
+  const decodedToken = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decodedToken));
+  //Check for expired tokens
+  const currentTime = Date.now() / 1000;
+  if (decodedToken.exp < currentTime) {
+    // log out user
+    store.dispatch(logoutUser());
+    // Set profile to null
+    // store.dispatch(clearProfile());
+    // clear current Profile
+    // TODO:::TODO
+    // Redirect to login
+    window.alert("Session expired, please login again.");
+    window.location.href = "/login";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Provider store={store}>
+          <Router>
+            <Route exact path="/" render={() => <Navbar />} />
+            <Route exact path="/login" render={() => <Login />} />
+            <Route exact path="/register" render={() => <Register />} />
+            <Route exact path="/dashboard" render={() => <Dashboard />} />
+          </Router>
+        </Provider>
+        <h1> </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
