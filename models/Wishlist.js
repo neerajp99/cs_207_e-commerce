a4fa1604566403a8586e5a2ce38eac567f68a1a1
neerajp99 <neerajp1999@gmail.com>
2019-10-12 21:36:42 +0530

    Add wishlist model

---
+++ models/Wishlist.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WishlistSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  productId: {
+    type: String,
+    isRequired: true
+  }
+});
+
+module.exports = Wishlist = mongoose.model("cart", WishlistSchema);
