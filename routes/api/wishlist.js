cc2ef03fe78715470b48dab47d9c1d8a2a4bdafe
neerajp99 <neerajp1999@gmail.com>
2019-10-13 01:21:59 +0530

    Modify wishlist route

--- routes/api/wishlist.js
+++ routes/api/wishlist.js
@@ -3,47 +3,34 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Wishlist = require("../../models/Wishlist");
-const Profile = require("../../models/Profile");
-const Item = require("../../models/Item");
 
 // @route POST /api/wishlist/
 // @description post to user wishlist
 // @access Private
 router.post(
-  "/:id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOne({
-      user: req.user.id
-    }).then(profile => {
-      if (profile) {
-        Item.findOne({
-          id: req.params.id
-        }).then(item => {
-          if (item) {
-            Wishlist.findOne({
-              productId: item.id
-            }).then(wish => {
-              if (wish) {
-                res.status(400).json("Item already there in wishlist");
-              }
-
-              const newWish = new Wishlist({
-                user: req.user.id,
-                productId: item.id
-              });
-
-              newWish
-                .save()
-                .then(productWish => {
-                  res.json(productWish);
-                })
-                .catch(error => {
-                  res.status(400).json(error);
-                });
-            });
-          }
+    console.log(req.user.id);
+    Wishlist.findOne({
+      productId: req.body.id
+    }).then(wish => {
+      if (wish) {
+        return res.status(400).json("Item already there in wishlist");
+      } else {
+        const newWish = new Wishlist({
+          user: req.user.id,
+          productId: req.body.id
         });
+
+        newWish
+          .save()
+          .then(productWish => {
+            res.json(productWish);
+          })
+          .catch(error => {
+            res.status(400).json(error);
+          });
       }
     });
   }
@@ -67,6 +54,5 @@ router.get(
       });
   }
 );
-
 
 module.exports = router;
