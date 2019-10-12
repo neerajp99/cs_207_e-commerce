969566a7063c73b5b14087d355d40647fbc94af9
neerajp99 <neerajp1999@gmail.com>
2019-10-13 05:03:56 +0530

    Edit Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -14,6 +14,33 @@ const CartSchema = new Schema({
     type: Number,
     min: 1,
     isRequired: true
+  },
+  product: {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users"
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true
+    },
+    size: {
+      type: [String],
+      required: true
+    }
   }
 });
 
