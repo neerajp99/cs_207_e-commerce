25e881c301642818cc55a0ec68831055b7790055
neerajp99 <neerajp1999@gmail.com>
2019-10-13 05:04:05 +0530

    Add global model

--- models/Item.js
+++ models/Item.js
@@ -1,53 +1,58 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ItemSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "users"
+const ItemSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users"
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      min: 0,
+      max: 1000000,
+      required: true
+    },
+    size: {
+      type: [String],
+      required: true
+    }
+    // details: [
+    //   {
+    //     specialPrice: {
+    //       type: String
+    //     },
+    //     specialDiscount: {
+    //       type: String
+    //     }
+    //   }
+    // ],
+    // extraDetails: [
+    //   {
+    //     highlights: {
+    //       type: String
+    //     },
+    //     services: {
+    //       type: String
+    //     }
+    //   }
+    // ]
   },
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    min: 0,
-    max: 1000000,
-    required: true
-  },
-  size: {
-    type: [String],
-    required: true
-  }
-  // details: [
-  //   {
-  //     specialPrice: {
-  //       type: String
-  //     },
-  //     specialDiscount: {
-  //       type: String
-  //     }
-  //   }
-  // ],
-  // extraDetails: [
-  //   {
-  //     highlights: {
-  //       type: String
-  //     },
-  //     services: {
-  //       type: String
-  //     }
-  //   }
-  // ]
-});
+  { strict: false }
+);
 
-module.exports = Item = mongoose.model("items", ItemSchema);
+global.ItemSchema = global.ItemSchema || mongoose.model('items', ItemSchema);
+module.exports = global.ItemSchema;
+// module.exports = Item = mongoose.model("items", ItemSchema);
