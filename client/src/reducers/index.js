ab5ca7f2e4e568f6c232b5e4cc01aacf74b8274a
neerajp99 <neerajp1999@gmail.com>
2019-10-12 21:38:44 +0530

    Edit root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import itemReducer from "./itemReducer"
 
 export default combineReducers({
   auth: authReducer,
-  error: errorReducer
+  error: errorReducer,
+
 });
