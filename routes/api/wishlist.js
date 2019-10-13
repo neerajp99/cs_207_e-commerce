211b2acb74db44718ec48e1052c335dcd457f450
neerajp99 <neerajp1999@gmail.com>
2019-10-13 08:22:50 +0530

    Add move to cart route

--- routes/api/wishlist.js
+++ routes/api/wishlist.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Wishlist = require("../../models/Wishlist");
+const Cart = require("../../models/Cart");
 
 // @route POST /api/wishlist/
 // @description post to user wishlist
@@ -12,18 +13,37 @@ router.post(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     console.log(req.user.id);
+    const updateFields = {};
+    updateFields.user = req.user.id;
+
+    if (req.body.productId) {
+      updateFields.productId = req.body.productId;
+    }
+
+    updateFields.product = {};
+    if (req.body.name) {
+      updateFields.product.name = req.body.name;
+    }
+    if (req.body.description) {
+      updateFields.product.description = req.body.description;
+    }
+    if (req.body.size) {
+      updateFields.product.size = req.body.size.split(",");
+    }
+    if (req.body.category) {
+      updateFields.product.category = req.body.category;
+    }
+    if (req.body.price) {
+      updateFields.product.price = req.body.price;
+    }
+
     Wishlist.findOne({
       productId: req.body.id
     }).then(wish => {
       if (wish) {
         return res.status(400).json("Item already there in wishlist");
       } else {
-        const newWish = new Wishlist({
-          user: req.user.id,
-          productId: req.body.id
-        });
-
-        newWish
+        new Wishlist(updateFields)
           .save()
           .then(productWish => {
             res.json(productWish);
@@ -52,6 +72,92 @@ router.get(
       .catch(error => {
         res.status(400).json(error);
       });
+  }
+);
+
+// @route DELETE api/wishlist/:cart_id
+// @description Delete wishlist item
+// @access Private
+router.delete(
+  "/:wishlist_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Wishlist.deleteOne({ productId: req.params.wishlist_id }, error => {
+      if (!error) {
+        Wishlist.find({ user: req.user.id }).then(product => {
+          res.json(product);
+        });
+      } else {
+        return res.status(400).json(error);
+      }
+    });
+  }
+);
+
+// @route DELETE api/wishlist/:cart_id
+// @description Move wishlist item to cart and remove from wishlist
+// @access Private
+router.delete(
+  "/moveToCart/:wishlist_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Wishlist.findOne({
+      productId: req.params.wishlist_id
+    }).then(wish => {
+      const updateFields = {};
+      updateFields.user = req.user.id;
+
+      updateFields.productId = wish.productId;
+
+      updateFields.quantity = 1;
+
+      updateFields.product = {};
+
+      updateFields.product.name = wish.product.name;
+
+      updateFields.product.description = wish.product.description;
+
+      updateFields.product.size = wish.product.size;
+
+      updateFields.product.category = wish.product.category;
+
+      updateFields.product.price = wish.product.price;
+
+      Cart.findOne({
+        productId: req.params.wishlist_id
+      })
+        .then(cart => {
+          new Cart(updateFields)
+
+            .save()
+            .then(productCart => {
+              res.json(productCart);
+            })
+            .then(() => {
+              Wishlist.deleteOne(
+                { productId: req.params.wishlist_id },
+                error => {
+                  if (!error) {
+                    Wishlist.find({ user: req.user.id }).then(product => {
+                      res.json(product);
+                    });
+                  } else {
+                    return res.status(400).json(error);
+                  }
+                }
+              );
+            })
+
+            .catch(error => {
+              console.log("hahahhaa");
+              res.status(400).json(error);
+            });
+        })
+        .catch(error => {
+          console.log("jajajaajhaa");
+          res.status(400).json(error);
+        });
+    });
   }
 );
 
