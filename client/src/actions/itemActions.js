1b01ba1ff5fb79c2e399e676b515861b6099aff7
neerajp99 <neerajp1999@gmail.com>
2019-10-15 15:56:46 +0530

    Add getItem action

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,4 +1,4 @@
-import { GET_ITEM, GET_ERRORS } from "./types";
+import { GET_ITEMS, GET_ERRORS } from "./types";
 import axios from "axios";
 
 // Post item
@@ -8,6 +8,24 @@ export const postItem = (data, history) => dispatch => {
     .then(res => {
       history.push("/dashboard");
       console.log(res);
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
+    });
+};
+
+// Get Item
+export const getItems = () => dispatch => {
+  axios
+    .get("/api/item")
+    .then(res => {
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      });
     })
     .catch(error => {
       dispatch({
