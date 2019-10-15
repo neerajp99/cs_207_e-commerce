73e93499131e856841eb3e08ce440eabb14978d2
neerajp99 <neerajp1999@gmail.com>
2019-10-15 15:56:24 +0530

    Edit action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
-export const GET_ITEM = "GET_ITEM"
+export const GET_ITEMS = "GET_ITEMS"
