d28d1d1a0433c0207dfe8b64b69f41449c4a2af8
neerajp99 <neerajp1999@gmail.com>
2019-10-16 17:26:19 +0530

    Add get item by id action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,4 @@ export const GET_ERRORS = "GET_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const GET_ITEMS = "GET_ITEMS"
+export const GET_ITEM_BY_ID = "GET_ITEM_BY_ID"
