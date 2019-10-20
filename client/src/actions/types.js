ae472b64a0bda33c314a56e3b99c1d4d32e187d8
neerajp99 <neerajp1999@gmail.com>
2019-10-20 19:12:17 +0530

    Modify action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,3 +5,4 @@ export const GET_ITEMS = "GET_ITEMS";
 export const GET_ITEM_BY_ID = "GET_ITEM_BY_ID";
 export const ADD_TO_CART = "ADD_TO_CART";
 export const GET_CART_ITEMS = "GET_CART_ITEMS";
+export const GET_WISHLIST_ITEMS = "GET_WISHLIST_ITEMS"
