e5e51178ca43bbdfdd73f795c57a97091d88d8a2
neerajp99 <neerajp1999@gmail.com>
2019-10-20 22:17:36 +0530

    Edit product page

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -8,11 +8,31 @@ import { addToCart } from "../../actions/cartActions";
 import { addToWishlist } from "../../actions/wishlistActions";
 
 class Product extends Component {
+  state = {
+    selectedSize: "",
+    six: "shoe_size_list_item",
+    seven: "shoe_size_list_item",
+    eight: "shoe_size_list_item",
+    nine: "shoe_size_list_item",
+    ten: "shoe_size_list_item"
+  };
   componentDidMount() {
     if (this.props.match.params.id) {
       this.props.getItemById(this.props.match.params.id);
     }
   }
+  onClickSize = event => {
+    console.log(event.target.label);
+    this.setState({
+      selectedSize: [event.target.value],
+      six: "shoe_size_list_item",
+      seven: "shoe_size_list_item",
+      eight: "shoe_size_list_item",
+      nine: "shoe_size_list_item",
+      ten: "shoe_size_list_item",
+      [event.target.type]: "shoe_size_list_item checked_size"
+    });
+  };
   onClickCart = event => {
     event.preventDefault();
     if (this.props.items.item) {
@@ -20,7 +40,7 @@ class Product extends Component {
         name: this.props.items.item.name,
         description: this.props.items.item.description,
         category: this.props.items.item.category,
-        size: this.props.items.item.size,
+        size: this.state.selectedSize,
         price: this.props.items.item.price,
         productId: this.props.items.item._id,
         quantity: 1
@@ -35,7 +55,7 @@ class Product extends Component {
         name: this.props.items.item.name,
         description: this.props.items.item.description,
         category: this.props.items.item.category,
-        size: this.props.items.item.size,
+        size: this.state.selectedSize,
         price: this.props.items.item.price,
         productId: this.props.items.item._id
       };
@@ -56,25 +76,70 @@ class Product extends Component {
             <div className="col-md-6 product-right">
               <h3 className="brand">JORDAN</h3>
               <hr />
-              <h2 className="product-name">AIR JORDAN XXXIV PF 'WHITE'</h2>
-              <h3 className="product-price">$699</h3>
+              <h2 className="product-name">{item.name}</h2>
+              <h3 className="product-price">${item.price}</h3>
               <h6 className="description">Description</h6>
               <hr />
-              <h6 className="product-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </h6>
+              <h6 className="product-description">{item.description}</h6>
+              <br />
+              <ul className="shoe_size_list">
+                <li
+                  className={this.state.six}
+                  value="6"
+                  type="six"
+                  onClick={this.onClickSize}
+                >
+                  6
+                </li>
+                <li
+                  className={this.state.seven}
+                  value="7"
+                  type="seven"
+                  onClick={this.onClickSize}
+                >
+                  7
+                </li>
+                <li
+                  className={this.state.eight}
+                  value="8"
+                  type="eight"
+                  onClick={this.onClickSize}
+                >
+                  8
+                </li>
+                <li
+                  className={this.state.nine}
+                  value="9"
+                  type="nine"
+                  onClick={this.onClickSize}
+                >
+                  9
+                </li>
+                <li
+                  className={this.state.ten}
+                  value="10"
+                  type="ten"
+                  onClick={this.onClickSize}
+                >
+                  10
+                </li>
+              </ul>
               <ul className="prod-list">
-                <li className="list-item">
-                  <button className="prod-button">Add to Cart</button>
+                <li className="prod-list-item">
+                  <button
+                    className=" prod-button btn-dark btn btn-block mt-4"
+                    onClick={this.onClickCart}
+                  >
+                    Add to Cart
+                  </button>
                 </li>
-                <li className="list-item">
-                  <button className="prod-button">Wishlist</button>
-                </li>
-                <li className="list-item">
-                  <button className="prod-button">Quantity</button>
+                <li className="prod-list-item">
+                  <button
+                    className=" prod-button btn-dark btn btn-block mt-4"
+                    onClick={this.onClickWishlist}
+                  >
+                    Wishlist
+                  </button>
                 </li>
               </ul>
             </div>
