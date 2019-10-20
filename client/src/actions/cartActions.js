0966b025f2708f9fa8b56f0fb9fef3aa6c9e7ce1
neerajp99 <neerajp1999@gmail.com>
2019-10-20 19:12:32 +0530

    Modify cart Action

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -20,11 +20,43 @@ export const getCartItems = () => dispatch => {
   axios
     .get("/api/cart")
     .then(res => {
-      console.log(res)
+      console.log(res);
       dispatch({
         type: GET_CART_ITEMS,
         payload: res.data
       });
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
+    });
+};
+
+// delete cartItem
+export const deleteCart = id => dispatch => {
+  axios
+    .delete(`/api/cart/${id}`)
+    .then(res => {
+      console.log(res);
+      window.location.reload();
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
+    });
+};
+
+// add to wishlist
+export const cartToWishlist = id => dispatch => {
+  axios
+    .delete(`api/cart/moveToWishlist/${id}`)
+    .then(res => {
+      console.log(res);
+      window.location.reload();
     })
     .catch(error => {
       dispatch({
