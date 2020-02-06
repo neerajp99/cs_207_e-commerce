758189cf850c9e2dcd0964df47d565c6a947c89d
neerajp99 <neerajp1999@gmail.com>
2020-02-06 21:56:57 +0530

    Add mongoose keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://<username>:<user password>@ds019916.mlab.com:19916/e-commerce",
+  mongoURI: "mongodb://neeraj:neeraj123@ds019916.mlab.com:19916/e-commerce",
   secretOrKey: 'iamaprogrammer'
 }
